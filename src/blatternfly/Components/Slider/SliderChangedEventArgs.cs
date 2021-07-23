1315c9bfc20a463a54a24bfd18a9ccdf3cc6e64a
carlosga <carlos.guzman.1@outlook.es>
2021-07-23 19:54:55 +0200

    New slider component

---
+++ src/blatternfly/Components/Slider/SliderChangedEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Blatternfly.Components
+{
+    public sealed class SliderChangedEventArgs : EventArgs
+    {
+        public decimal Value { get; set; }
+        public decimal InputValue { get; set; }
+    }
+}
---
+++ src/blatternfly/Components/Slider/SliderStepObject.cs
@@ -0,0 +1,14 @@
+﻿namespace Blatternfly.Components
+{
+    public sealed class SliderStepObject
+    {
+        /// Value of the step. This value is a percentage of the slider where the tick is drawn.
+        public decimal Value { get; set; }
+        
+        /// The display label for the step value. This is also used for the aria-valuetext.
+        public string Label { get; set; }
+        
+        /// Flag to hide the label.
+        public bool IsLabelHidden { get; set; }        
+    }
+}
