c79ff675b6e0949f68b1863a27dd4a9eea032264
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2021-07-29 23:26:45 +0200

    Dropdown Toggle onKeyDown port ( using JS interop )

--- src/blatternfly/wwwroot/blatternfly.js
+++ src/blatternfly/wwwroot/blatternfly.js
@@ -10,5 +10,36 @@ window.Blatternfly = {
     },
     windowInnerWidth: function () {
         return window.innerWidth;
+    },
+}
+
+window.Blatternfly.Dropdown = {
+    onKeyDown: function (dotNetObjRef, toggleId) {
+        function keydownHandler(event) {
+            const state = dotNetObjRef.invokeMethod("KeydownState");
+            if (event.key === 'Tab' && !state.isOpen) {
+                return;
+            }
+            if ((event.key === 'Tab' || event.key === 'Enter' || event.key === ' ') && state.isOpen) {
+                if (!state.bubbleEvent) {
+                    event.stopPropagation();
+                }
+                event.preventDefault();
+
+                if (event.key !== ' ') {
+                    dotNetObjRef.invokeMethod("KeydownOnToggle");   
+                }
+            } else if ((event.key === 'Enter' || event.key === 'ArrowDown' || event.key === ' ') && !state.isOpen) {
+                if (!state.bubbleEvent) {
+                    event.stopPropagation();
+                }
+                event.preventDefault();
+
+                if (event.key !== ' ') {
+                    dotNetObjRef.invokeMethod("KeyDownOnEnter"); // onToggle + onEnter   
+                }
+            }
+        }
+        document.getElementById(toggleId).addEventListener("keydown", keydownHandler);
     }
 }
