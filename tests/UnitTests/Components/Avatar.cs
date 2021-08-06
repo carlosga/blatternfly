8d902927f6cf636771d60fb7b2c7c8873d664c2d
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2021-08-06 17:24:50 +0200

    · Avatar test ( ported from patternfly ) using bUnit ( https://bunit.dev/ ) · Added unit test project

---
+++ tests/UnitTests/Components/Avatar.cs
@@ -0,0 +1,33 @@
+using Xunit;
+using Bunit;
+using Blatternfly.Components;
+
+namespace Blatternfly.UnitTests.Components
+{
+    public class AvatarGroupTests
+    {
+        [Fact]
+        public void SimpleAvatarTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<Avatar>(parameters => parameters
+                .Add(p => p.AlternateText, "avatar")
+                .Add(p => p.Source       , "test.png")
+                .Add(p => p.Border       , AvatarBorder.Light)
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<img
+  alt=""avatar""
+  class=""pf-c-avatar pf-m-light""
+  src=""test.png""
+/>
+");
+        }
+    }
+}
