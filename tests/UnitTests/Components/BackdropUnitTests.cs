dba6a66bef2141e3cc8ca4441136cdb843d3708d
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2021-08-06 17:37:31 +0200

    Backdrop and Badge tests ported from patternfly

---
+++ tests/UnitTests/Components/BackdropUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using Bunit;
+using Blatternfly.Components;
+
+namespace Blatternfly.UnitTests.Components
+{
+    public class BackdropUnitTests
+    {
+        [Fact]
+        public void BackdropTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<Backdrop>(parameters => parameters
+                .AddChildContent("Backdrop")
+            );
+
+            // Assert
+            cut.MarkupMatches(
+                @"
+<div 
+  class=""pf-c-backdrop""
+>
+  Backdrop
+</div>
+");
+        }
+    }
+}
