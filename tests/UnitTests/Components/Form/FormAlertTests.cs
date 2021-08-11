02c7025b0a1aeb153bb21cb78e5d956191db7a09
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2021-08-11 22:57:25 +0200

    Form Tests

---
+++ tests/UnitTests/Components/Form/FormAlertTests.cs
@@ -0,0 +1,22 @@
+﻿using Blatternfly.Components;
+using Bunit;
+using Xunit;
+
+namespace Blatternfly.UnitTests.Components
+{
+    public class FormAlertTests
+    {
+        [Fact]
+        public void DefaultTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<FormAlert>();
+
+            // Assert
+            cut.MarkupMatches(@"<div class=""pf-c-form__alert"" />");
+        }
+    }
+}
---
+++ tests/UnitTests/Components/Form/FormFieldGroup.cs
@@ -0,0 +1,70 @@
+﻿using Blatternfly.Components;
+using Bunit;
+using Xunit;
+
+namespace Blatternfly.UnitTests.Components
+{
+    public class FormFieldGroupTests
+    {
+        [Fact]
+        public void DefaultTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<FormFieldGroup>(parameters => parameters
+                .Add<FormFieldGroupHeader>(p => p.Header, headerparams => headerparams
+                    .Add(p => p.TitleText, "Field group 4 (non-expandable)")
+                    .Add(p => p.TitleTextId, "title-text-id1")
+                    .Add(p => p.TitleDescription, "Field group 4 description text.")
+                    .Add<Button>(p => p.Actions)
+                )
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<div
+  class=""pf-c-form__field-group""
+>
+  <div
+    class=""pf-c-form__field-group-header""
+  >
+    <div
+      class=""pf-c-form__field-group-header-main""
+    >
+      <div
+        class=""pf-c-form__field-group-header-title""
+      >
+        <div
+          class=""pf-c-form__field-group-header-title-text""
+          id=""title-text-id1""
+        >
+          Field group 4 (non-expandable)
+        </div>
+      </div>
+      <div
+        class=""pf-c-form__field-group-header-description""
+      >
+        Field group 4 description text.
+      </div>
+    </div>
+    <div
+      class=""pf-c-form__field-group-header-actions""
+    >
+      <button
+        aria-disabled=""false"" 
+        class=""pf-c-button pf-m-primary""
+        type=""button""
+      />
+    </div>
+  </div>
+  <div
+    class=""pf-c-form__field-group-body""
+  />
+</div>
+");
+        }
+    }
+}
