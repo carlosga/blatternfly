c1d3b9530be6c86c2efe38c07303789bfd874daa
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2021-08-13 18:16:22 +0200

    LoginMainHeader tests

---
+++ tests/UnitTests/Components/LoginPage/LoginMainHeaderTests.cs
@@ -0,0 +1,91 @@
+﻿using Blatternfly.Components;
+using Bunit;
+using Xunit;
+
+namespace Blatternfly.UnitTests.Components
+{
+    public class LoginMainHeaderTests
+    {
+        [Fact]
+        public void DefaultTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<LoginMainHeader>();
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<header
+  class=""pf-c-login__main-header""
+/>
+");
+        }      
+        
+        [Fact]
+        public void WithAdditionalCssClassTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<LoginMainHeader>(parameters => parameters
+                .AddUnmatched("class", "extra-class")
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<header
+  class=""pf-c-login__main-header extra-class""
+/>
+");
+        }
+        
+        [Fact]
+        public void WithAdditionalPropertiesOnRootElementTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var testId = "login-header";
+
+            // Act
+            var cut = ctx.RenderComponent<LoginMainHeader>(parameters => parameters
+                .AddUnmatched("data-testid", testId)
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@$"
+<header
+  class=""pf-c-login__main-header""
+  data-testid=""{testId}""
+/>
+");
+        }   
+        
+        [Fact]
+        public void WithTitleAndSubtitleTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<LoginMainHeader>(parameters => parameters
+                .Add(p => p.Title, "Log in to your account")
+                .Add(p => p.Subtitle, "Use LDAP credentials")
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<header class=""pf-c-login__main-header"">
+  <h2 class=""pf-c-title pf-m-3xl"">Log in to your account</h2>
+  <p class=""pf-c-login__main-header-desc"">Use LDAP credentials</p>
+</header>
+");
+        }        
+    }
+}
