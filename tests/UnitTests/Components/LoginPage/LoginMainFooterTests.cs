5b559e3f4f30fa983b28cd8e9cef7e691e4ced8d
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2021-08-13 19:17:19 +0200

    LoginMainFooter Tests

---
+++ tests/UnitTests/Components/LoginPage/LoginMainFooterTests.cs
@@ -0,0 +1,71 @@
+﻿using Blatternfly.Components;
+using Bunit;
+using Xunit;
+
+namespace Blatternfly.UnitTests.Components
+{
+    public class LoginMainFooterTests
+    {
+        [Fact]
+        public void DefaultTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<LoginMainFooter>();
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<div
+  class=""pf-c-login__main-footer""
+/>
+");
+        }      
+        
+        [Fact]
+        public void WithAdditionalCssClassTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<LoginMainFooter>(parameters => parameters
+                .AddUnmatched("class", "extra-class")
+
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<div
+  class=""pf-c-login__main-footer extra-class""
+/>
+");
+        }           
+        
+        [Fact]
+        public void WithAdditionalPropertiesOnRootElementTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var testId = "login-body";
+
+            // Act
+            var cut = ctx.RenderComponent<LoginMainFooter>(parameters => parameters
+                .AddUnmatched("data-testid", testId)
+
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@$"
+<div
+  class=""pf-c-login__main-footer"" 
+  data-testid=""{testId}""
+/>
+");
+        }
+    }
+}
