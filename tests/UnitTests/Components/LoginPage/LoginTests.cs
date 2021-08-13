a3e2b91dcfb0c9417f5c519a162803e1d196fb99
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2021-08-13 19:32:40 +0200

    Login Tests

---
+++ tests/UnitTests/Components/LoginPage/LoginTests.cs
@@ -0,0 +1,52 @@
+﻿using Blatternfly.Components;
+using Bunit;
+using Xunit;
+
+namespace Blatternfly.UnitTests.Components
+{
+    public class LoginTests
+    {
+        [Fact]
+        public void DefaultTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<Login>(parameters => parameters
+                .Add<LoginFooter>(p => p.Footer, footerparams => footerparams
+                    .AddChildContent("Footer")
+                )
+                .Add<LoginHeader>(p => p.Header, headerparams => headerparams
+                    .Add(p => p.HeaderBrand, "HeaderBrand")
+                    .AddChildContent("Header Text")
+                )
+                .AddChildContent("Main")
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<div
+  class=""pf-c-login""
+>
+  <div
+    class=""pf-c-login__container""
+  >
+  <header class=""pf-c-login__header"">
+    HeaderBrand   Header Text
+  </header>
+  <main
+      class=""pf-c-login__main""
+    >
+      Main
+    </main>
+    <footer class=""pf-c-login__footer"">
+      Footer
+    </footer>
+  </div>
+</div>
+");
+        }        
+    }
+}
