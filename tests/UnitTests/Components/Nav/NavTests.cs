da2606462caa3abe06b84fc892930c953670c1dd
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2021-08-14 12:18:45 +0200

    Nav tests

--- tests/UnitTests/Components/Nav/NavTests.cs
+++ tests/UnitTests/Components/Nav/NavTests.cs
@@ -293,5 +293,191 @@ namespace Blatternfly.UnitTests.Components
 </nav>
 ");
         }
+        
+        [Fact]
+        public void NavGroupedListTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<Nav>(parameters => parameters
+                .Add<NavGroup>(p => p.ChildContent, groupparams1 => groupparams1
+                    .AddUnmatched("id", "grp-1")
+                    .Add(p => p.Title, "Section 1")
+                    .Add<NavList>(p => p.ChildContent, listparams => listparams
+                        .AddItems(s_items)
+                    )
+                )
+                .Add<NavGroup>(p => p.ChildContent, groupparams1 => groupparams1
+                    .AddUnmatched("id", "grp-2")
+                    .Add(p => p.Title, "Section 2")
+                    .Add<NavList>(p => p.ChildContent, listparams => listparams
+                        .AddItems(s_items)
+                    )
+                )
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<nav 
+  class=""pf-c-nav"" 
+  aria-label=""Global"" 
+>
+  <section class=""pf-c-nav__section"" aria-labelledby=""grp-1"">
+    <h2 class=""pf-c-nav__section-title"" id=""grp-1"">Section 1</h2>
+    <ul class=""pf-c-nav__list"">
+      <ul class=""pf-c-nav__list"">
+        <li class=""pf-c-nav__item""><a href=""#link1"" class=""pf-c-nav__link"">Link 1</a></li>
+        <li class=""pf-c-nav__item""><a href=""#link2"" class=""pf-c-nav__link"">Link 2</a></li>
+        <li class=""pf-c-nav__item""><a href=""#link3"" class=""pf-c-nav__link"">Link 3</a></li>
+        <li class=""pf-c-nav__item""><a href=""#link4"" class=""pf-c-nav__link"">Link 4</a></li>
+      </ul>
+    </ul>
+  </section>
+  <section class=""pf-c-nav__section"" aria-labelledby=""grp-2"">
+    <h2 class=""pf-c-nav__section-title"" id=""grp-2"">Section 2</h2>
+    <ul class=""pf-c-nav__list"">
+      <ul class=""pf-c-nav__list"">
+        <li class=""pf-c-nav__item""><a href=""#link1"" class=""pf-c-nav__link"">Link 1</a></li>
+        <li class=""pf-c-nav__item""><a href=""#link2"" class=""pf-c-nav__link"">Link 2</a></li>
+        <li class=""pf-c-nav__item""><a href=""#link3"" class=""pf-c-nav__link"">Link 3</a></li>
+        <li class=""pf-c-nav__item""><a href=""#link4"" class=""pf-c-nav__link"">Link 4</a></li>
+      </ul>
+    </ul>
+  </section>
+</nav>
+");
+        }        
+        
+        [Fact]
+        public void HorizontalNavListTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<Nav>(parameters => parameters
+                .Add(p => p.Variant, NavVariant.Horizontal)
+                .Add<NavList>(p => p.ChildContent, listparams => listparams
+                    .AddItems(s_items)
+                )
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@$"
+<nav
+  class=""pf-c-nav pf-m-horizontal""
+  aria-label=""Global""
+>
+  <button
+    class=""pf-c-nav__scroll-button""
+    aria-label=""Scroll left""
+    disabled=""""
+  >
+    <svg
+      style=""vertical-align: -0.125em;"" 
+      fill=""currentColor"" 
+      height=""1em"" 
+      width=""1em"" 
+      viewBox=""{AngleLeftIcon.IconDefinition.ViewBox}"" 
+      aria-hidden=""true"" 
+      role=""img""
+    >
+      <path d=""{AngleLeftIcon.IconDefinition.SvgPath}""></path>
+    </svg>  
+  </button>
+  <ul class=""pf-c-nav__list"">
+    <li class=""pf-c-nav__item""><a href=""#link1"" class=""pf-c-nav__link"">Link 1</a></li>
+    <li class=""pf-c-nav__item""><a href=""#link2"" class=""pf-c-nav__link"">Link 2</a></li>
+    <li class=""pf-c-nav__item""><a href=""#link3"" class=""pf-c-nav__link"">Link 3</a></li>
+    <li class=""pf-c-nav__item""><a href=""#link4"" class=""pf-c-nav__link"">Link 4</a></li>
+  </ul>
+  <button
+    class=""pf-c-nav__scroll-button""
+    aria-label=""Scroll right""
+    disabled=""""
+  >
+    <svg
+      style=""vertical-align: -0.125em;"" 
+      fill=""currentColor"" 
+      height=""1em"" 
+      width=""1em"" 
+      viewBox=""{AngleRightIcon.IconDefinition.ViewBox}"" 
+      aria-hidden=""true"" 
+      role=""img""
+    >
+      <path d=""{AngleRightIcon.IconDefinition.SvgPath}""></path>
+    </svg>      
+  </button>
+</nav>
+");
+        }
+        
+        [Fact]
+        public void TertiaryNavListTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<Nav>(parameters => parameters
+                .Add(p => p.Variant, NavVariant.Tertiary)
+                .Add<NavList>(p => p.ChildContent, listparams => listparams
+                    .AddItems(s_items)
+                )
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@$"
+<nav
+  class=""pf-c-nav pf-m-horizontal pf-m-tertiary""
+  aria-label=""Local""
+>
+  <button
+    class=""pf-c-nav__scroll-button""
+    aria-label=""Scroll left""
+    disabled=""""
+  >
+    <svg
+      style=""vertical-align: -0.125em;"" 
+      fill=""currentColor"" 
+      height=""1em"" 
+      width=""1em"" 
+      viewBox=""{AngleLeftIcon.IconDefinition.ViewBox}"" 
+      aria-hidden=""true"" 
+      role=""img""
+    >
+      <path d=""{AngleLeftIcon.IconDefinition.SvgPath}""></path>
+    </svg>  
+  </button>
+  <ul class=""pf-c-nav__list"">
+    <li class=""pf-c-nav__item""><a href=""#link1"" class=""pf-c-nav__link"">Link 1</a></li>
+    <li class=""pf-c-nav__item""><a href=""#link2"" class=""pf-c-nav__link"">Link 2</a></li>
+    <li class=""pf-c-nav__item""><a href=""#link3"" class=""pf-c-nav__link"">Link 3</a></li>
+    <li class=""pf-c-nav__item""><a href=""#link4"" class=""pf-c-nav__link"">Link 4</a></li>
+  </ul>
+  <button
+    class=""pf-c-nav__scroll-button""
+    aria-label=""Scroll right""
+    disabled=""""
+  >
+    <svg
+      style=""vertical-align: -0.125em;"" 
+      fill=""currentColor"" 
+      height=""1em"" 
+      width=""1em"" 
+      viewBox=""{AngleRightIcon.IconDefinition.ViewBox}"" 
+      aria-hidden=""true"" 
+      role=""img""
+    >
+      <path d=""{AngleRightIcon.IconDefinition.SvgPath}""></path>
+    </svg>      
+  </button>
+</nav>");
+        }
     }
 }
