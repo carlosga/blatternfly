ac41736c5ff634c4ea6181230379617d09e815ff
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2021-08-14 19:13:39 +0200

    NotificationDrawerList Tests

---
+++ tests/UnitTests/Components/NotificationDrawer/NotificationDrawerListTests.cs
@@ -0,0 +1,90 @@
+﻿using Blatternfly.Components;
+using Bunit;
+using Xunit;
+
+namespace Blatternfly.UnitTests.Components
+{
+    public class NotificationDrawerListTests
+    {
+        [Fact]
+        public void DefaultTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<NotificationDrawerList>();
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<ul
+  class=""pf-c-notification-drawer__list""
+/>
+");
+        }
+        
+        [Fact]
+        public void WithAdditionalCssClassTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<NotificationDrawerList>(parameters => parameters
+                .AddUnmatched("class", "extra-class")
+            );
+
+            // Assert
+            cut.MarkupMatches(
+                @"
+<ul
+  class=""pf-c-notification-drawer__list extra-class""
+/>
+");
+        }    
+        
+        [Fact]
+        public void WithAdditionalPropertiesOnRootElementTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var testId = "notification-drawer";
+
+            // Act
+            var cut = ctx.RenderComponent<NotificationDrawerList>(parameters => parameters
+                .AddUnmatched("data-testid", testId)
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@$"
+<ul
+  class=""pf-c-notification-drawer__list""
+  data-testid=""{testId}""
+/>
+");
+        }          
+        
+        [Fact]
+        public void IsHiddenTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<NotificationDrawerList>(parameters => parameters
+                .Add(p => p.IsHidden, true)
+            );
+
+            // Assert
+            cut.MarkupMatches(
+                @$"
+<ul
+  class=""pf-c-notification-drawer__list""
+  hidden=""""
+/>
+");
+        }         
+    }
+}
