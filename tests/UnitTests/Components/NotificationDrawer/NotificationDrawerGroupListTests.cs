35815cb333685e4acd57e1720ebf4d3dbabefea1
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2021-08-14 21:08:20 +0200

    NotificationDrawerGroupList Tests

---
+++ tests/UnitTests/Components/NotificationDrawer/NotificationDrawerGroupListTests.cs
@@ -0,0 +1,69 @@
+﻿using Blatternfly.Components;
+using Bunit;
+using Xunit;
+
+namespace Blatternfly.UnitTests.Components
+{
+    public class NotificationDrawerGroupListTests
+    {
+        [Fact]
+        public void DefaultTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<NotificationDrawerGroupList>();
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<div
+  class=""pf-c-notification-drawer__group-list""
+/>
+");
+        }
+        
+        [Fact]
+        public void WithAdditionalCssClassTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<NotificationDrawerGroupList>(parameters => parameters
+                .AddUnmatched("class", "extra-class")
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<div
+  class=""pf-c-notification-drawer__group-list extra-class""
+/>
+");
+        }        
+        
+        [Fact]
+        public void WithAdditionalPropertiesOnRootElementTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var testId = "notification-drawer";
+
+            // Act
+            var cut = ctx.RenderComponent<NotificationDrawerGroupList>(parameters => parameters
+                .AddUnmatched("data-testid", testId)
+            );
+
+            // Assert
+            cut.MarkupMatches(
+$@"
+<div
+  class=""pf-c-notification-drawer__group-list""
+  data-testid=""{testId}""
+/>
+");
+        }          
+    }
+}
