33b69b2e81363675d900a08ba73f9a14337cdde7
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2021-08-14 22:28:04 +0200

    NotificationDrawer Tests

---
+++ tests/UnitTests/Components/NotificationDrawer/NotificationDrawerBodyTests.cs
@@ -0,0 +1,69 @@
+﻿using Blatternfly.Components;
+using Bunit;
+using Xunit;
+
+namespace Blatternfly.UnitTests.Components
+{
+    public class NotificationDrawerBodyTests
+    {
+        [Fact]
+        public void DefaultTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<NotificationDrawerBody>();
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<div
+  class=""pf-c-notification-drawer__body""
+/>
+");
+        }
+        
+        [Fact]
+        public void WithAdditionalCssClassTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<NotificationDrawerBody>(parameters => parameters
+                .AddUnmatched("class", "extra-class")
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<div
+  class=""pf-c-notification-drawer__body extra-class""
+/>
+");
+        }   
+        
+        [Fact]
+        public void WithAdditionalPropertiesOnRootElementTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var testId = "notification-drawer";
+
+            // Act
+            var cut = ctx.RenderComponent<NotificationDrawerBody>(parameters => parameters
+                .AddUnmatched("data-testid", testId)
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@$"
+<div
+  class=""pf-c-notification-drawer__body""
+  data-testid=""{testId}""
+/>
+");
+        }  
+    }
+}
---
+++ tests/UnitTests/Components/NotificationDrawer/NotificationDrawerTests.cs
@@ -0,0 +1,69 @@
+﻿using Blatternfly.Components;
+using Bunit;
+using Xunit;
+
+namespace Blatternfly.UnitTests.Components
+{
+    public class NotificationDrawerTests
+    {
+        [Fact]
+        public void DefaultTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<NotificationDrawer>();
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<div
+  class=""pf-c-notification-drawer""
+/>
+");
+        }
+        
+        [Fact]
+        public void WithAdditionalCssClass()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<NotificationDrawer>(parameters => parameters
+                .AddUnmatched("class", "extra-class")
+            );
+
+            // Assert
+            cut.MarkupMatches(
+@"
+<div
+  class=""pf-c-notification-drawer extra-class""
+/>
+");
+        }
+        
+        [Fact]
+        public void WithAdditionalPropertiesOnRootElementTest()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var testId = "notification-drawer";
+
+            // Act
+            var cut = ctx.RenderComponent<NotificationDrawer>(parameters => parameters
+                .AddUnmatched("data-testid", testId)
+            );
+
+            // Assert
+            cut.MarkupMatches(
+                @$"
+<div
+  class=""pf-c-notification-drawer""
+  data-testid=""{testId}""
+/>
+");
+        }          
+    }
+}
