b9e4270da9bab668bb76cffece21bccf62831c66
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2021-08-21 23:47:43 +0200

    asset paths changes

--- src/blatternfly.demo/Shared/Paths.cs
+++ src/blatternfly.demo/Shared/Paths.cs
@@ -3,5 +3,6 @@ namespace Blatternfly.Demo.Shared
     internal static class Paths
     {
         internal const string Assets = "assets/images";
+        internal const string BlatternflyAssets = "/_content/Blatternfly/assets/images";
     }
 }
