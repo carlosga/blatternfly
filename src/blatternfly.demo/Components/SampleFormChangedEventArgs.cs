37fd691f0014e3a3ca47c65f187af31360e543bf
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2021-09-24 21:02:25 +0200

    Wizard Component (WIP)

---
+++ src/blatternfly.demo/Components/SampleFormChangedEventArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace Blatternfly.Demo.Components
+{
+    public class SampleFormChangedEventArgs
+    {
+        public bool IsValid { get; set; }
+        public string Value { get; set; }
+    }
+}
