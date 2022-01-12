96d896cbf86b7a38f56bb889fb06fbb2d7bb2185
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-01-12 19:27:47 +0100

    PatternFly Sync - Toggle Group Component Allow to disable all items under the parent ToggleGroup

--- tests/UnitTests/Components/ToggleGroup/ToggleGroupTests.cs
+++ tests/UnitTests/Components/ToggleGroup/ToggleGroupTests.cs
@@ -22,23 +22,71 @@ public class ToggleGroupTests
         // Assert
         cut.MarkupMatches(
 @"
-<div 
-  class=""pf-c-toggle-group pf-m-compact"" 
+<div
+  class=""pf-c-toggle-group pf-m-compact""
   role=""group""
 >
   <div class=""pf-c-toggle-group__item"">
-    <button 
-      type=""button"" 
-      class=""pf-c-toggle-group__button"" 
+    <button
+      type=""button""
+      class=""pf-c-toggle-group__button""
       aria-pressed=""false""
     >
       <span class=""pf-c-toggle-group__text"">Test</span>
     </button>
   </div>
   <div class=""pf-c-toggle-group__item"">
-    <button 
-      type=""button"" 
-      class=""pf-c-toggle-group__button"" 
+    <button
+      type=""button""
+      class=""pf-c-toggle-group__button""
+      aria-pressed=""false""
+    >
+      <span class=""pf-c-toggle-group__text"">Test</span>
+    </button>
+  </div>
+</div>
+");
+    }
+
+    [Fact]
+    public void AreAllGroupsDisabledTest()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+
+        // Act
+        var cut = ctx.RenderComponent<ToggleGroup>(properties => properties
+            .Add(p => p.AreAllGroupsDisabled, true)
+            .Add<ToggleGroupItem>(p => p.ChildContent, itemparams1 => itemparams1
+                .Add(p => p.Text, "Test")
+            )
+            .Add<ToggleGroupItem>(p => p.ChildContent, itemparams2 => itemparams2
+                .Add(p => p.Text, "Test")
+            )
+        );
+
+        // Assert
+        cut.MarkupMatches(
+@"
+<div
+  class=""pf-c-toggle-group""
+  role=""group""
+>
+  <div class=""pf-c-toggle-group__item"">
+    <button
+      type=""button""
+      class=""pf-c-toggle-group__button""
+      disabled=""""
+      aria-pressed=""false""
+    >
+      <span class=""pf-c-toggle-group__text"">Test</span>
+    </button>
+  </div>
+  <div class=""pf-c-toggle-group__item"">
+    <button
+      type=""button""
+      class=""pf-c-toggle-group__button""
+      disabled=""""
       aria-pressed=""false""
     >
       <span class=""pf-c-toggle-group__text"">Test</span>
