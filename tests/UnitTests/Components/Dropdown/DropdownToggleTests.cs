0b867bc247c5b94761b8dce5c198441af3453bf5
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-01-12 19:03:30 +0100

    PatternFly Sync : Dropdown Component Added plain-text- variant

--- tests/UnitTests/Components/Dropdown/DropdownToggleTests.cs
+++ tests/UnitTests/Components/Dropdown/DropdownToggleTests.cs
@@ -7,7 +7,7 @@ public class DropdownToggleTests
     {
         // Arrange
         using var ctx = new TestContext();
-        
+
         // Setup Javascript interop
         ctx.SetupJavascriptInterop();
 
@@ -49,9 +49,9 @@ $@"
     </svg>
   </span>
 </button>
-");            
+");
     }
-    
+
     [Fact]
     public void IsActiveTest()
     {
@@ -100,6 +100,160 @@ $@"
     </svg>
   </span>
 </button>
-");            
-    }            
+");
+    }
+
+    [Fact]
+    public void ButtonVariantPrimaryTest()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+
+        // Setup Javascript interop
+        ctx.SetupJavascriptInterop();
+
+        // Act
+        var cut = ctx.RenderComponent<DropdownToggle>(parameters => parameters
+            .AddUnmatched("id", "Dropdown Toggle")
+            .Add(p => p.ToggleVariant, ToggleVariant.Primary)
+            .AddChildContent("Dropdown")
+        );
+
+        // Assert
+        cut.MarkupMatches(
+$@"
+<button
+  aria-expanded=""false""
+  class=""pf-c-dropdown__toggle pf-m-primary""
+  id=""Dropdown Toggle""
+  type=""button""
+>
+  <span
+    class=""pf-c-dropdown__toggle-text""
+  >
+    Dropdown
+  </span>
+  <span
+    class=""pf-c-dropdown__toggle-icon""
+  >
+    <svg
+      aria-hidden=""true""
+      fill=""currentColor""
+      height=""1em""
+      role=""img""
+      style=""vertical-align: -0.125em""
+      viewBox=""{CaretDownIcon.IconDefinition.ViewBox}""
+      width=""1em""
+    >
+      <path
+        d=""{CaretDownIcon.IconDefinition.SvgPath}""
+      />
+    </svg>
+  </span>
+</button>
+");
+    }
+
+    [Fact]
+    public void ButtonVariantSecondaryTest()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+
+        // Setup Javascript interop
+        ctx.SetupJavascriptInterop();
+
+        // Act
+        var cut = ctx.RenderComponent<DropdownToggle>(parameters => parameters
+            .AddUnmatched("id", "Dropdown Toggle")
+            .Add(p => p.ToggleVariant, ToggleVariant.Secondary)
+            .AddChildContent("Dropdown")
+        );
+
+        // Assert
+        cut.MarkupMatches(
+$@"
+<button
+  aria-expanded=""false""
+  class=""pf-c-dropdown__toggle pf-m-secondary""
+  id=""Dropdown Toggle""
+  type=""button""
+>
+  <span
+    class=""pf-c-dropdown__toggle-text""
+  >
+    Dropdown
+  </span>
+  <span
+    class=""pf-c-dropdown__toggle-icon""
+  >
+    <svg
+      aria-hidden=""true""
+      fill=""currentColor""
+      height=""1em""
+      role=""img""
+      style=""vertical-align: -0.125em""
+      viewBox=""{CaretDownIcon.IconDefinition.ViewBox}""
+      width=""1em""
+    >
+      <path
+        d=""{CaretDownIcon.IconDefinition.SvgPath}""
+      />
+    </svg>
+  </span>
+</button>
+");
+    }
+
+    [Fact]
+    public void IsTextTest()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+
+        // Setup Javascript interop
+        ctx.SetupJavascriptInterop();
+
+        // Act
+        var cut = ctx.RenderComponent<DropdownToggle>(parameters => parameters
+            .AddUnmatched("id", "Dropdown Toggle")
+            .Add(p => p.IsText, true)
+            .Add(p => p.IsPlain, true)
+            .AddChildContent("Dropdown")
+        );
+
+        // Assert
+        cut.MarkupMatches(
+$@"
+<button
+  aria-expanded=""false""
+  class=""pf-c-dropdown__toggle pf-m-plain pf-m-text""
+  id=""Dropdown Toggle""
+  type=""button""
+>
+  <span
+    class=""pf-c-dropdown__toggle-text""
+  >
+    Dropdown
+  </span>
+  <span
+    class=""pf-c-dropdown__toggle-icon""
+  >
+    <svg
+      aria-hidden=""true""
+      fill=""currentColor""
+      height=""1em""
+      role=""img""
+      style=""vertical-align: -0.125em""
+      viewBox=""{CaretDownIcon.IconDefinition.ViewBox}""
+      width=""1em""
+    >
+      <path
+        d=""{CaretDownIcon.IconDefinition.SvgPath}""
+      />
+    </svg>
+  </span>
+</button>
+");
+    }
 }
