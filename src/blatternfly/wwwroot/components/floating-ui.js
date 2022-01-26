a132691c5b50125afe9cb8fda725fb6102cb4625
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-01-26 21:34:14 +0100

    Tooltip Component (WIP)

--- src/blatternfly/wwwroot/components/floating-ui.js
+++ src/blatternfly/wwwroot/components/floating-ui.js
@@ -6,33 +6,31 @@ import {
 } from 'https://cdn.skypack.dev/@floating-ui/dom@0.2.0';
 
 export async function computeFloatingPosition(referenceId, floatingId, options) {
-  window.requestAnimationFrame(() => {
-    options = options || {};
+  options = options || {};
 
-    const referenceEl = document.querySelector(`#${referenceId}`);
-    const floatingEl  = document.querySelector(`#${floatingId}`);
+  const referenceEl = document.querySelector(`#${referenceId}`);
+  const floatingEl  = document.querySelector(`#${floatingId}`);
 
-    options.middleware = [
-      offset(options.distance),
-      shift(),
-    ];
+  options.middleware = [
+    offset(options.distance),
+    shift(),
+  ];
 
-    if (options.enableFlip === true) {
-      options.middleware.push(flip({
-        fallbackPlacements: options.fallbackPlacements
-      }));
-    }
+  if (options.enableFlip === true) {
+    options.middleware.push(flip({
+      fallbackPlacements: options.fallbackPlacements
+    }));
+  }
 
+  Object.assign(floatingEl.style, {
+    position: 'absolute',
+  });
+
+  computePosition(referenceEl, floatingEl, options).then(({x, y}) => {
     Object.assign(floatingEl.style, {
-      position: 'absolute',
-    });
-
-    computePosition(referenceEl, floatingEl, options).then(({x, y}) => {
-      Object.assign(floatingEl.style, {
-        top: '0',
-        left: '0',
-        transform: `translate3d(${Math.round(x)}px,${Math.round(y)}px,0)`
-      });
+      top: '0',
+      left: '0',
+      transform: `translate3d(${Math.round(x)}px,${Math.round(y)}px,0)`
     });
   });
 }
