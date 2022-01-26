dca6c8157c49a381a47de6fdcb67e669b5a62271
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-01-26 18:20:39 +0100

    Tooltip component (WIP)

--- src/blatternfly/wwwroot/components/floating-ui.js
+++ src/blatternfly/wwwroot/components/floating-ui.js
@@ -2,52 +2,37 @@ import {
   computePosition,
   offset,
   shift,
-  flip,
-  // getScrollParents
+  flip
 } from 'https://cdn.skypack.dev/@floating-ui/dom@0.2.0';
 
-
 export async function computeFloatingPosition(referenceId, floatingId, options) {
-  options = options || {};
+  window.requestAnimationFrame(() => {
+    options = options || {};
 
-  const referenceEl = document.querySelector(`#${referenceId}`);
-  const floatingEl  = document.querySelector(`#${floatingId}`);
+    const referenceEl = document.querySelector(`#${referenceId}`);
+    const floatingEl  = document.querySelector(`#${floatingId}`);
 
-  options.middleware = [
-    offset(options.distance),
-    shift(),
-  ];
+    options.middleware = [
+      offset(options.distance),
+      shift(),
+    ];
 
-  if (options.enableFlip === true) {
-    options.middleware.push(flip({
-      fallbackPlacements: options.fallbackPlacements
-    }));
-  }
+    if (options.enableFlip === true) {
+      options.middleware.push(flip({
+        fallbackPlacements: options.fallbackPlacements
+      }));
+    }
 
-  Object.assign(floatingEl.style, {
-    position: 'absolute',
-  });
-
-  async function update() {
-    const {x, y} = await computePosition(referenceEl, floatingEl, options);
     Object.assign(floatingEl.style, {
-      top: '0',
-      left: '0',
-      transform: `translate3d(${Math.round(x)}px,${Math.round(y)}px,0)`
+      position: 'absolute',
     });
-  }
 
-  await update(referenceEl, floatingEl, options);
-
-  // Adds event listeners to `window`
-  // addEventListener('scroll', update);
-  // addEventListener('resize', update);
-
-  // [
-  //   ...getScrollParents(referenceEl),
-  //   ...getScrollParents(floatingEl),
-  // ].forEach((el) => {
-  //   el.addEventListener('scroll', update);
-  //   el.addEventListener('resize', update);
-  // });
+    computePosition(referenceEl, floatingEl, options).then(({x, y}) => {
+      Object.assign(floatingEl.style, {
+        top: '0',
+        left: '0',
+        transform: `translate3d(${Math.round(x)}px,${Math.round(y)}px,0)`
+      });
+    });
+  });
 }
