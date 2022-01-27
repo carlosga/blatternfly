d4904512dfdf3741ac5d0f3e86d846f2a778d41a
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-01-27 15:55:52 +0100

    Portal component changes

--- src/blatternfly/Components/Portal/IPortalConnector.cs
+++ src/blatternfly/Components/Portal/IPortalConnector.cs
@@ -1,11 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace Blatternfly.Components;
 
 public interface IPortalConnector
 {
+    IObservable<Portal> OnConnect    { get; }
+    IObservable<Portal> OnDisconnect { get; }
+
     void Attach(PortalHost host);
     void Detach();
-    ValueTask ConnectAsync(Portal portal);
-    ValueTask DisconnectAsync();
+    void Connect(Portal portal);
+    void Disconnect(Portal portal);
 }
