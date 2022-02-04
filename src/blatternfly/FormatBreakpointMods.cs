cd724847b2d44715b55e5b51c89ef12fb914336a
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-02-04 19:10:19 +0100

    PatternFly Sync : FormatBreakpointMods

--- src/blatternfly/FormatBreakpointMods.cs
+++ src/blatternfly/FormatBreakpointMods.cs
@@ -2,6 +2,16 @@
 
 public abstract class FormatBreakpointMods<T>
 {
+    private static readonly Breakpoints[] s_BreakpointsOrder =
+    {
+        Breakpoints.ExtraLarge2,
+        Breakpoints.ExtraLarge,
+        Breakpoints.Large,
+        Breakpoints.Medium,
+        Breakpoints.Small,
+        Breakpoints.Default
+    };
+
     public T Default { get; set; }
     public T Small { get; set; }
     public T Medium { get; set; }
@@ -26,17 +36,64 @@ public abstract class FormatBreakpointMods<T>
             return null;
         }
 
+        if (breakpoint.HasValue)
+        {
+            var isBreakpointInMods = IsBreakpointInMods(breakpoint.Value);
+
+            if (isBreakpointInMods)
+            {
+                return CssClass(breakpoint.Value);
+            }
+
+            // the current breakpoint is not specified in mods, so we try to find the next nearest
+            var breakpointsIndex =
+                breakpoint.HasValue
+                    ? System.Array.IndexOf<Breakpoints>(s_BreakpointsOrder, breakpoint.Value)
+                        : -1;
+
+            for (var i = breakpointsIndex; i < s_BreakpointsOrder.Length; i++)
+            {
+                if (IsBreakpointInMods(s_BreakpointsOrder[i]))
+                {
+                    return CssClass(s_BreakpointsOrder[i]);
+                }
+            }
+            return null;
+        }
+
         return new CssBuilder()
-            .AddClass(() => $"pf-{Prefix}-{ToString(Default)}"           , Default     is not null && (breakpoint is null || breakpoint.Value == Breakpoints.Default))
-            .AddClass(() => $"pf-{Prefix}-{ToString(Small)}-on-sm"       , Small       is not null && (breakpoint is null || breakpoint.Value == Breakpoints.Small))
-            .AddClass(() => $"pf-{Prefix}-{ToString(Medium)}-on-md"      , Medium      is not null && (breakpoint is null || breakpoint.Value == Breakpoints.Medium))
-            .AddClass(() => $"pf-{Prefix}-{ToString(Large)}-on-lg"       , Large       is not null && (breakpoint is null || breakpoint.Value == Breakpoints.Large))
-            .AddClass(() => $"pf-{Prefix}-{ToString(ExtraLarge)}-on-xl"  , ExtraLarge  is not null && (breakpoint is null || breakpoint.Value == Breakpoints.ExtraLarge))
-            .AddClass(() => $"pf-{Prefix}-{ToString(ExtraLarge2)}-on-2xl", ExtraLarge2 is not null && (breakpoint is null || breakpoint.Value == Breakpoints.ExtraLarge2))
+            .AddClass(() => $"pf-{Prefix}-{ToString(Default)}"           , Default     is not null)
+            .AddClass(() => $"pf-{Prefix}-{ToString(Small)}-on-sm"       , Small       is not null)
+            .AddClass(() => $"pf-{Prefix}-{ToString(Medium)}-on-md"      , Medium      is not null)
+            .AddClass(() => $"pf-{Prefix}-{ToString(Large)}-on-lg"       , Large       is not null)
+            .AddClass(() => $"pf-{Prefix}-{ToString(ExtraLarge)}-on-xl"  , ExtraLarge  is not null)
+            .AddClass(() => $"pf-{Prefix}-{ToString(ExtraLarge2)}-on-2xl", ExtraLarge2 is not null)
+            .Build();
+    }
+
+    protected string CssClass(Breakpoints breakpoint)
+    {
+        return new CssBuilder()
+            .AddClass(() => $"pf-{Prefix}-{ToString(Default)}"    , breakpoint == Breakpoints.Default)
+            .AddClass(() => $"pf-{Prefix}-{ToString(Small)}"      , breakpoint == Breakpoints.Small)
+            .AddClass(() => $"pf-{Prefix}-{ToString(Medium)}"     , breakpoint == Breakpoints.Medium)
+            .AddClass(() => $"pf-{Prefix}-{ToString(Large)}"      , breakpoint == Breakpoints.Large)
+            .AddClass(() => $"pf-{Prefix}-{ToString(ExtraLarge)}" , breakpoint == Breakpoints.ExtraLarge)
+            .AddClass(() => $"pf-{Prefix}-{ToString(ExtraLarge2)}", breakpoint == Breakpoints.ExtraLarge2)
             .Build();
     }
 
     protected abstract string Prefix { get; }
 
     protected abstract string ToString(T value);
+
+    private bool IsBreakpointInMods(Breakpoints breakpoint)
+    {
+        return Default     is not null && breakpoint == Breakpoints.Default
+            || Small       is not null && breakpoint == Breakpoints.Small
+            || Medium      is not null && breakpoint == Breakpoints.Medium
+            || Large       is not null && breakpoint == Breakpoints.Large
+            || ExtraLarge  is not null && breakpoint == Breakpoints.ExtraLarge
+            || ExtraLarge2 is not null && breakpoint == Breakpoints.ExtraLarge2;
+    }
 }
