5d8b396309035a59814b68aeedccef41338751e8
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-02-18 18:57:50 +0100

    Cleanup

--- src/blatternfly/Components/Floating/FloatingOptions.cs
+++ src/blatternfly/Components/Floating/FloatingOptions.cs
@@ -1,18 +1,12 @@
-using System.Text.Json.Serialization;
-
 namespace Blatternfly.Components;
 
 public sealed class FloatingOptions
 {
-    [JsonPropertyName("placement")]
     public TooltipPosition Placement { get; set; }
 
-    [JsonPropertyName("distance")]
     public int Distance { get; set; }
 
-    [JsonPropertyName("enableFlip")]
     public bool EnableFlip { get; set; }
 
-    [JsonPropertyName("fallbackPlacements")]
     public TooltipPosition[] FallbackPlacements { get; set; }
 }
--- src/blatternfly/wwwroot/components/floating-ui.js
+++ src/blatternfly/wwwroot/components/floating-ui.js
@@ -1,9 +1,4 @@
-import {
-  computePosition,
-  offset,
-  // shift,
-  // flip
-} from '../third-party/floating-ui/floating-ui.dom.esm.min.js';
+import { computePosition, offset/*, shift, flip*/ } from '../third-party/floating-ui/floating-ui.dom.esm.min.js';
 
 export async function computeFloatingPosition(referenceId, floatingId, placement, distance, enableFlip, fallbackPlacements) {
   const referenceEl = document.querySelector(`#${referenceId}`);
@@ -14,10 +9,7 @@ export async function computeFloatingPosition(referenceId, floatingId, placement
     distance: distance,
     enableFlip: enableFlip,
     fallbackPlacements: fallbackPlacements,
-    middleware: [
-      offset(distance),
-      // shift(),
-    ]
+    middleware: [offset(distance)/*, shift(),*/]
   };
 
   // if (options.enableFlip === true) {
