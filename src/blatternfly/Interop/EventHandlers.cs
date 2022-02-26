447189fa0c307a9eaa1133bf3bc9c54488933cee
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-02-27 00:02:51 +0100

    - Tooltips (WIP) -
    · Updated Floating UI version
    · Fire tooltips using mouse enter/leave events instead of mouse over/out

--- src/blatternfly/Interop/EventHandlers.cs
+++ src/blatternfly/Interop/EventHandlers.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-
-namespace Blatternfly.Interop;
-
-[EventHandler("onmouseenter", typeof(MouseEventArgs), true, true)]
+﻿[EventHandler("onmouseenter", typeof(MouseEventArgs), true, true)]
 [EventHandler("onmouseleave", typeof(MouseEventArgs), true, true)]
 public static class EventHandlers
 {
