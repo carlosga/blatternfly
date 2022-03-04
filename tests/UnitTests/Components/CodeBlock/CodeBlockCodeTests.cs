0abd61d02f43ff70bd6f1da4b37faeed48cccf67
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-03-04 14:06:27 +0100

    CodeBlock Tests

---
+++ tests/UnitTests/Components/CodeBlock/CodeBlockCodeTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Blatternfly.UnitTests.Components;
+
+public class CodeBlockCodeTests
+{
+    [Fact(Skip="bunit")]
+    public void DefaultTest()
+    {
+        // Arrange
+        using var ctx = Helper.CreateTestContext();
+
+        // Act
+        var cut = ctx.RenderComponent<CodeBlockCode>(parameters => parameters
+            .AddChildContent("action")
+        );
+
+        // Assert
+        cut.MarkupMatches(
+@"
+<pre
+  class=""pf-c-code-block__pre""
+>
+  <code
+    class=""pf-c-code-block__code""
+  >
+    action
+  </code>
+</pre>
+");
+    }
+}
