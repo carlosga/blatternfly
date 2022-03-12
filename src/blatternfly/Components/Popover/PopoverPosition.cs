d912492bfb634470ffef36b6cc0ad73d36e02260
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-03-12 23:47:08 +0100

    Popover component (WIP)

--- src/blatternfly/Components/Popover/PopoverPosition.cs
+++ src/blatternfly/Components/Popover/PopoverPosition.cs
@@ -1,14 +1,24 @@
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Text.Json.Serialization;
 using Blatternfly.Converters;
 
-namespace Blatternfly.Components;
-
-[JsonConverter(typeof(EnumDescriptionConverter<PopoverPosition>))]
-public enum PopoverPosition
+namespace Blatternfly.Components
 {
-  [Description("top")] Top,
-  [Description("bottom")] Bottom,
-  [Description("left")] Left,
-  [Description("right")] Right,
-};
+    [JsonConverter(typeof(EnumDescriptionConverter<PopoverPosition>))]
+    public enum PopoverPosition
+    {
+        [Description("auto")] Auto,
+        [Description("top")] Top,
+        [Description("top-start")] TopStart,
+        [Description("top-end")] TopEnd,
+        [Description("bottom")] Bottom,
+        [Description("bottom-start")] BottomStart,
+        [Description("bottom-end")] BottomEnd,
+        [Description("right")] Right,
+        [Description("right-start")] RightStart,
+        [Description("right-end")] RightEnd,
+        [Description("left")] Left,
+        [Description("left-start")] LeftStart,
+        [Description("left-end")] LeftEnd
+    }
+}
