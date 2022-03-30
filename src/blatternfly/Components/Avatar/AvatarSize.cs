eeceed87be1869c2589f1399b789614aac287741
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-03-30 20:16:22 +0200

    PatternFly Sync : Avatar

---
+++ src/blatternfly/Components/Avatar/AvatarSize.cs
@@ -0,0 +1,9 @@
+namespace Blatternfly.Components;
+
+public enum AvatarSize
+{
+    Small,
+    Medium,
+    Large,
+    ExtraLarge
+}
--- tests/UnitTests/Components/Avatar/AvatarTests.cs
+++ tests/UnitTests/Components/Avatar/AvatarTests.cs
@@ -10,9 +10,9 @@ public class AvatarTests
 
         // Act
         var cut = ctx.RenderComponent<Avatar>(parameters => parameters
-            .Add(p => p.Alt, "avatar")
-            .Add(p => p.Src       , "test.png")
-            .Add(p => p.Border       , AvatarBorder.Light)
+            .Add(p => p.Alt   , "avatar")
+            .Add(p => p.Src   , "test.png")
+            .Add(p => p.Border, AvatarBorder.Light)
         );
 
         // Assert
@@ -21,6 +21,44 @@ public class AvatarTests
 <img
   alt=""avatar""
   class=""pf-c-avatar pf-m-light""
+  src=""test.png""
+/>
+");
+    }
+
+    [Theory]
+    [InlineData(AvatarSize.Small)]
+    [InlineData(AvatarSize.Medium)]
+    [InlineData(AvatarSize.Large)]
+    [InlineData(AvatarSize.ExtraLarge)]
+    public void SizeVariationsTest(AvatarSize size)
+    {
+        // Arrange
+        using var ctx = Helper.CreateTestContext();
+
+        var sizeClass = size switch
+        {
+            AvatarSize.Small      => "pf-m-sm",
+            AvatarSize.Medium     => "pf-m-md",
+            AvatarSize.Large      => "pf-m-lg",
+            AvatarSize.ExtraLarge => "pf-m-xl",
+            _                     => ""
+        };
+
+        // Act
+        var cut = ctx.RenderComponent<Avatar>(parameters => parameters
+            .Add(p => p.Alt   , "avatar")
+            .Add(p => p.Src   , "test.png")
+            .Add(p => p.Border, AvatarBorder.Light)
+            .Add(p => p.Size  , size)
+        );
+
+        // Assert
+        cut.MarkupMatches(
+$@"
+<img
+  alt=""avatar""
+  class=""pf-c-avatar pf-m-light {sizeClass}""
   src=""test.png""
 />
 ");
