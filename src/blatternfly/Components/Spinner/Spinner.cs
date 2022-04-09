81bfbcf400f56195e5d1d8df073b2dcd4e49cc30
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-04-09 19:51:09 +0200

    Build Render Tree sequence normalization ( zero based )

--- src/blatternfly/Components/Spinner/Spinner.cs
+++ src/blatternfly/Components/Spinner/Spinner.cs
@@ -21,69 +21,68 @@ public class Spinner : BaseComponent
     [Parameter] public string AriaLabelledBy { get; set; }
 
     private string CssClass => new CssBuilder("pf-c-spinner")
-        .AddClass("pf-m-sm", Size == SpinnerSize.Small)
-        .AddClass("pf-m-md", Size == SpinnerSize.Medium)
-        .AddClass("pf-m-lg", Size == SpinnerSize.Large)
-        .AddClass("pf-m-xl", Size == SpinnerSize.ExtraLarge)
+        .AddClass("pf-m-sm", Size is SpinnerSize.Small)
+        .AddClass("pf-m-md", Size is SpinnerSize.Medium)
+        .AddClass("pf-m-lg", Size is SpinnerSize.Large)
+        .AddClass("pf-m-xl", Size is SpinnerSize.ExtraLarge)
         .AddClassFromAttributes(AdditionalAttributes)
         .Build();
 
     protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
-        var index     = 1;
         var component = IsSvg ? "svg" : "span";
 
-        builder.OpenElement(index++, component);
-        builder.AddMultipleAttributes(index++, AdditionalAttributes);
-        builder.AddAttribute(index++, "aria-valuetext", AriaValueText);
-        builder.AddAttribute(index++, "class", CssClass);
-        builder.AddAttribute(index++, "role", "progressbar");
+        builder.OpenElement(0, component);
+        builder.AddMultipleAttributes(1, AdditionalAttributes);
+        builder.AddAttribute(2, "aria-valuetext", AriaValueText);
+        builder.AddAttribute(3, "class", CssClass);
+        builder.AddAttribute(4, "role", "progressbar");
 
         if (IsSvg)
         {
-            builder.AddAttribute(index++, "viewBox", "0 0 100 100");
+            builder.AddAttribute(5, "viewBox", "0 0 100 100");
         }
         if (!string.IsNullOrEmpty(Diameter))
         {
-            builder.AddAttribute(index++, "style", $"--pf-c-spinner--diameter:{Diameter}");
+            builder.AddAttribute(6, "style", $"--pf-c-spinner--diameter:{Diameter}");
         }
 
         if (!string.IsNullOrEmpty(AriaLabel))
         {
-            builder.AddAttribute(index++, "aria-label", AriaLabel);
+            builder.AddAttribute(7, "aria-label", AriaLabel);
         }
         if (!string.IsNullOrEmpty(AriaLabelledBy))
         {
-            builder.AddAttribute(index++, "aria-labelledBy", AriaLabelledBy);
+            builder.AddAttribute(8, "aria-labelledBy", AriaLabelledBy);
         }
 
         if (string.IsNullOrEmpty(AriaLabel) && string.IsNullOrEmpty(AriaLabelledBy))
         {
-            builder.AddAttribute(index++, "aria-label", "Contents");
+            builder.AddAttribute(9, "aria-label", "Contents");
         }
 
         if (IsSvg)
         {
-            builder.OpenElement(index++, "circle");
-            builder.AddAttribute(index++, "class", "pf-c-spinner__path");
-            builder.AddAttribute(index++, "cx", "50");
-            builder.AddAttribute(index++, "cy", "50");
-            builder.AddAttribute(index++, "r", "45");
-            builder.AddAttribute(index++, "fill", "none");
+            builder.OpenElement(10, "circle");
+            builder.AddAttribute(11, "class", "pf-c-spinner__path");
+            builder.AddAttribute(12, "cx", "50");
+            builder.AddAttribute(13, "cy", "50");
+            builder.AddAttribute(14, "r", "45");
+            builder.AddAttribute(15, "fill", "none");
             builder.CloseElement();
         }
         else
         {
-            builder.OpenElement(index++, "span");
-            builder.AddAttribute(index++, "class", "pf-c-spinner__clipper");
+            builder.OpenElement(16, "span");
+            builder.AddAttribute(17, "class", "pf-c-spinner__clipper");
             builder.CloseElement();
 
-            builder.OpenElement(index++, "span");
-            builder.AddAttribute(index++, "class", "pf-c-spinner__lead-ball");
+            builder.OpenElement(18, "span");
+            builder.AddAttribute(19, "class", "pf-c-spinner__lead-ball");
             builder.CloseElement();
 
-            builder.OpenElement(index++, "span");
-            builder.AddAttribute(index++, "class", "pf-c-spinner__tail-ball");
+            builder.OpenElement(20, "span");
+            builder.AddAttribute(21, "class", "pf-c-spinner__tail-ball");
             builder.CloseElement();
         }
 
