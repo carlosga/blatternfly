a90fdc1fc22dc92e60002a06f493fbf059a9fa41
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-04-15 22:31:42 +0200

    PatternFly Sync : Brand Component

---
+++ src/blatternfly/Components/Brand/BrandHeightModifiers.cs
@@ -0,0 +1,6 @@
+namespace Blatternfly.Components;
+
+public sealed class BrandHeightModifiers : FormatBreakpointStyles<string>
+{
+    protected override string BaseStyle { get => "pf-c-brand--Height"; }
+}
---
+++ src/blatternfly/Components/Brand/BrandWidthModifiers.cs
@@ -0,0 +1,6 @@
+namespace Blatternfly.Components;
+
+public sealed class BrandWidthModifiers : FormatBreakpointStyles<string>
+{
+    protected override string BaseStyle { get => "pf-c-brand--Width"; }
+}
