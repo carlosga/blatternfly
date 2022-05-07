8051a1ea658d87e0ba9eed372086516459c26f24
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-05-07 09:22:00 +0200

    PatternFly Sync : Wizard

--- src/blatternfly/Components/Wizard/WizardStepChangedEventArgs.cs
+++ src/blatternfly/Components/Wizard/WizardStepChangedEventArgs.cs
@@ -2,8 +2,10 @@
 
 public sealed class WizardStepChangedEventArgs
 {
-    public int?   NewStep      { get; init; }
-    public string NewStepName  { get; set; }
-    public int?   PrevStep     { get; init; }
-    public string PrevStepName { get; init; }
+    public string NewStepId     { get; init; }
+    public int?   NewStepIndex  { get; init; }
+    public string NewStepName   { get; set; }
+    public string PrevStepId    { get; init; }
+    public int?   PrevStepIndex { get; init; }
+    public string PrevStepName  { get; init; }
 }
