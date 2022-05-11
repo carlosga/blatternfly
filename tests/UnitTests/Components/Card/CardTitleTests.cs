f5e67d10a7ed6bb72e8398d6e5b78b6e5d19a9d3
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-05-11 19:31:03 +0200

    PatternFly sync : Card component

---
+++ tests/UnitTests/Components/Card/CardTitleTests.cs
@@ -0,0 +1,73 @@
+﻿namespace Blatternfly.UnitTests.Components;
+
+public class CardTitleTests
+{
+    [Fact]
+    public void DefaultCardTest()
+    {
+        // Arrange
+        using var ctx = Helper.CreateTestContext();
+
+        // Act
+        var cut = ctx.RenderComponent<CardTitle>(parameters => parameters
+            .AddChildContent("text")
+        );
+
+        // Assert
+        cut.MarkupMatches(
+@"
+<div
+  class=""pf-c-card__title""
+>
+  text
+</div>
+");
+    }
+
+    [Fact]
+    public void WithCustomCssClassTest()
+    {
+        // Arrange
+        using var ctx = Helper.CreateTestContext();
+
+        // Act
+        var cut = ctx.RenderComponent<CardTitle>(parameters => parameters
+            .AddUnmatched("class", "extra-class")
+            .AddChildContent("text")
+        );
+
+        // Assert
+        cut.MarkupMatches(
+@"
+<div
+  class=""pf-c-card__title extra-class""
+>
+  text
+</div>
+");
+    }
+
+    [Fact]
+    public void WithAdditionalPropertiesTest()
+    {
+        // Arrange
+        using var ctx = Helper.CreateTestContext();
+
+        // Act
+        var cut = ctx.RenderComponent<CardTitle>(parameters => parameters
+            .AddUnmatched("data-testid", "card-header")
+            .AddChildContent("text")
+        );
+
+        // Assert
+        cut.MarkupMatches(
+@"
+<div
+  class=""pf-c-card__title""
+  data-testid=""card-header""
+>
+  text
+</div>
+");
+    }
+}
