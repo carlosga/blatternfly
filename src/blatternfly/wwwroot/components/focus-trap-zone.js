5b3963b82c17afe375b27a580ef8527d3d682e57
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-06-03 17:10:40 +0200

    Updated focus trap & tabbable versions

--- src/blatternfly/wwwroot/components/focus-trap-zone.js
+++ src/blatternfly/wwwroot/components/focus-trap-zone.js
@@ -1,4 +1,4 @@
-import { createFocusTrap } from '../third-party/focus-trap/focus-trap.esm.js'
+import { createFocusTrap } from '../third-party/focus-trap/focus-trap.esm.min.js'
 
 export function create(element, options) {
   return createFocusTrap(element, options);
