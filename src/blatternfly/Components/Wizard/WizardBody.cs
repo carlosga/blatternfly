fb7b847e702782834e6fcbe2e0bac720fe89e734
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-07-20 19:11:10 +0200

    PatternFly Sync : Wizard Component
    - Drawer support

--- src/blatternfly/Components/Wizard/WizardBody.cs
+++ src/blatternfly/Components/Wizard/WizardBody.cs
@@ -17,9 +17,23 @@ public class WizardBody : ComponentBase
     /// Component used as the primary content container.
     [Parameter] public string MainComponent { get; set; } = "div";
 
+    /// The currently active WizardStep
+    [Parameter] public WizardStep ActiveStep { get; set; }
+
+    /// Flag indicating whether the wizard body has a drawer.
+    [Parameter] public bool HasDrawer { get; set; }
+
+    /// Flag indicating the wizard drawer is expanded.
+    [Parameter] public bool IsDrawerExpanded { get; set; }
+
     private string MainBodyCssClass => new CssBuilder("pf-c-wizard__main-body")
         .AddClass("pf-m-no-padding", HasNoBodyPadding)
         .Build();
+
+    private bool WrapperWithDrawer
+    {
+        get => HasDrawer && ActiveStep is not null && ActiveStep.DrawerPanelContent is not null;
+    }
 
     protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
@@ -28,10 +42,30 @@ public class WizardBody : ComponentBase
         builder.AddAttribute(2, "aria-label", AriaLabel);
         builder.AddAttribute(3, "aria-labelledby", AriaLabelledby);
 
-        builder.OpenElement(4, "div");
-        builder.AddAttribute(5, "class", MainBodyCssClass);
-        builder.AddContent(6, ChildContent);
+        builder.OpenComponent<WizardDrawerWrapper>(4);
+        builder.AddAttribute(5, "HasDrawer", WrapperWithDrawer);
+        builder.AddAttribute(6, "Wrapper", (RenderFragment)delegate(RenderTreeBuilder innerBuilder)
+        {
+            innerBuilder.OpenComponent<Drawer>(7);
+            innerBuilder.AddAttribute(8, "IsInline", true);
+            innerBuilder.AddAttribute(9, "IsExpanded", IsDrawerExpanded);
+
+            innerBuilder.OpenComponent<DrawerContent>(10);
+
+            innerBuilder.AddAttribute(11, "PanelContent", ActiveStep.DrawerPanelContent);
+            innerBuilder.AddContent(12, ChildContent);
+
+            innerBuilder.CloseComponent();
+
+            innerBuilder.CloseComponent();
+        });
+
+        builder.OpenElement(13, "div");
+        builder.AddAttribute(14, "class", MainBodyCssClass);
+        builder.AddContent(15, ChildContent);
         builder.CloseElement();
+
+        builder.CloseComponent();
 
         builder.CloseElement();
     }
