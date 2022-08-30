09a37f1fc7ce9dc8a9e1959e925de3cb4e8af5d9
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-08-30 18:59:29 +0200

    Badge component
    - Code behind
    - XML doc comments

---
+++ src/blatternfly/Components/Badge/Badge.razor.cs
@@ -0,0 +1,28 @@
+﻿namespace Blatternfly.Components;
+
+public partial class Badge : ComponentBase
+{
+    /// <summary>
+    /// Additional attributes that will be applied to the component.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object> AdditionalAttributes { get; set; }
+
+    /// <summary>
+    /// Content rendered inside the component.
+    /// </summary>
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    /// <summary>
+    /// Adds styling to the badge to indicate it has been read.
+    /// </summary>
+    [Parameter]
+    public bool IsRead { get; set; }
+
+    private string CssClass => new CssBuilder("pf-c-badge")
+        .AddClass("pf-m-read", IsRead)
+        .AddClass("pf-m-unread", !IsRead)
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+}
