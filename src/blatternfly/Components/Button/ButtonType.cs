736c09ea03ab7986b28a35c3314668e3e1aa41ff
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-08-31 17:43:38 +0200

    Button component
    XML doc comments

--- src/blatternfly/Components/Button/ButtonType.cs
+++ src/blatternfly/Components/Button/ButtonType.cs
@@ -1,8 +1,22 @@
 namespace Blatternfly.Components;
 
+/// <summary>
+/// Button types
+/// </summary>
 public enum ButtonType
 {
+    /// <summary>
+    /// Button.
+    /// </summary>
     Button,
+
+    /// <summary>
+    /// Submit.
+    /// </summary>
     Submit,
+
+    /// <summary>
+    /// Reset.
+    /// </summary>
     Reset
 }
--- src/blatternfly/Components/Button/ButtonVariant.cs
+++ src/blatternfly/Components/Button/ButtonVariant.cs
@@ -1,14 +1,52 @@
 namespace Blatternfly.Components;
 
+/// <summary>
+/// Button variant styles.
+/// </summary>
 public enum ButtonVariant
 {
+    /// <summary>
+    /// Primary.
+    /// </summary>
     Primary,
+
+    /// <summary>
+    /// Secondary.
+    /// </summary>
     Secondary,
+
+    /// <summary>
+    /// Tertiary.
+    /// </summary>
     Tertiary,
+
+    /// <summary>
+    /// Danger.
+    /// </summary>
     Danger,
+
+    /// <summary>
+    /// Warning.
+    /// </summary>
     Warning,
+
+    /// <summary>
+    /// Link.
+    /// </summary>
     Link,
+
+    /// <summary>
+    /// Plain.
+    /// </summary>
     Plain,
+
+    /// <summary>
+    /// Inline.
+    /// </summary>
     Inline,
+
+    /// <summary>
+    /// Control
+    /// </summary>
     Control
 }
