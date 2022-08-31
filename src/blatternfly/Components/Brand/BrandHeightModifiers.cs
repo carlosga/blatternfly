bb670bcc2a89b8888511ddfda84ca8983d5ae0df
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-08-31 17:14:53 +0200

    Brand component
    - Code behind
    - XML Doc comments

--- src/blatternfly/Components/Brand/BrandHeightModifiers.cs
+++ src/blatternfly/Components/Brand/BrandHeightModifiers.cs
@@ -1,5 +1,8 @@
 namespace Blatternfly.Components;
 
+/// <summary>
+/// Heights breakpoint modifiers for <see cref="Brand" /> components.
+/// </summary>
 public sealed class BrandHeightModifiers : FormatBreakpointStyles<string>
 {
     protected override string BaseStyle { get => "pf-c-brand--Height"; }
--- src/blatternfly/Components/Brand/BrandWidthModifiers.cs
+++ src/blatternfly/Components/Brand/BrandWidthModifiers.cs
@@ -1,5 +1,8 @@
 namespace Blatternfly.Components;
 
+/// <summary>
+/// Widths breakpoint modifiers for <see cref="Brand" /> components.
+/// </summary>
 public sealed class BrandWidthModifiers : FormatBreakpointStyles<string>
 {
     protected override string BaseStyle { get => "pf-c-brand--Width"; }
