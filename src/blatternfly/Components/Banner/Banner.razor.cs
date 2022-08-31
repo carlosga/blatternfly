e8b04da43ec41ea013f1b1f3b7f0e194f710cfd2
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-08-31 17:12:02 +0200

    Inherit from ComponentBase

--- src/blatternfly/Components/Banner/Banner.razor.cs
+++ src/blatternfly/Components/Banner/Banner.razor.cs
@@ -1,6 +1,6 @@
 namespace Blatternfly.Components;
 
-public partial class Banner
+public partial class Banner : ComponentBase
 {
     /// <summary>
     /// Additional attributes that will be applied to the component.
