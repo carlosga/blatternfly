49c2a1d89aaf50b3c75b79acee49918b3a9af8d4
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-09-01 19:15:24 +0200

    CodeBlock component
    - Code behind
    - XML doc comments

---
+++ src/blatternfly/Components/CodeBlock/CodeBlock.razor.cs
@@ -0,0 +1,26 @@
+namespace Blatternfly.Components;
+
+public partial class CodeBlock : ComponentBase
+{
+    /// <summary>
+    /// Additional attributes that will be applied to the component.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object> AdditionalAttributes { get; set; }
+
+    /// <summary>
+    /// Content rendered inside the component.
+    /// </summary>
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    /// <summary>
+    /// Actions in the code block header. Should be wrapped with CodeBlockAction.
+    /// </summary>
+    [Parameter]
+    public RenderFragment Actions { get; set; }
+
+    private string CssClass => new CssBuilder("pf-c-code-block")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+}
