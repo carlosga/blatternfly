f4a7d775b5eabe39af677848930f7c366dec3018
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-09-01 19:35:47 +0200

    Divider component
    - Code Behind
    - XML doc comments

--- src/blatternfly/Components/Divider/Divider.cs
+++ src/blatternfly/Components/Divider/Divider.cs
@@ -2,16 +2,25 @@ namespace Blatternfly.Components;
 
 public class Divider : ComponentBase
 {
+    /// <summary>
     /// Additional attributes that will be applied to the component.
-    [Parameter(CaptureUnmatchedValues = true)] public IReadOnlyDictionary<string, object> AdditionalAttributes { get; set; }
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object> AdditionalAttributes { get; set; }
 
+    /// <summary>
     /// The component type to use.
+    /// </summary>
     [Parameter] public DividerVariant Component { get; set; } = DividerVariant.hr;
 
+    /// <summary>
     /// Insets at various breakpoints.
+    /// </summary>
     [Parameter] public InsetModifiers Inset { get; set; }
 
+    /// <summary>
     /// Indicates how the divider will display at various breakpoints. Vertical divider must be in a flex layout.
+    /// </summary>
     [Parameter] public OrientationModifiers Orientation { get; set; }
 
     private string CssClass => new CssBuilder("pf-c-divider")
--- src/blatternfly/Components/Divider/DividerVariant.cs
+++ src/blatternfly/Components/Divider/DividerVariant.cs
@@ -1,8 +1,22 @@
 namespace Blatternfly.Components;
 
+/// <summary>
+/// Divider variant.
+/// </summary>
 public enum DividerVariant
 {
+    /// <summary>
+    /// hr.
+    /// </summary>
     hr,
+
+    /// <summary>
+    /// li.
+    /// </summary>
     li,
+
+    /// <summary>
+    /// div.
+    /// </summary>
     div
 }
