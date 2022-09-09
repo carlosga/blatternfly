66c77bf0d4ee83279f0eeeffa75f82d116cbbb53
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-09-09 16:22:42 +0200

    PatternFly Sync : Page Component
    - Added a new HtmlElement component, to be used on components allowing to personalize the html tag to be rendered

---
+++ src/blatternfly/Components/HtmlElement.cs
@@ -0,0 +1,40 @@
+namespace Blatternfly.Components;
+
+public sealed class HtmlElement : ComponentBase
+{
+    /// <summary>
+    /// Additional attributes that will be applied to the component.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object> AdditionalAttributes { get; set; }
+
+    /// <summary>
+    /// Content rendered inside the component.
+    /// </summary>
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    /// <summary>
+    /// HTML element to render ( div, span, ul, li, ... )
+    /// </summary>
+    [Parameter]
+    public string Component { get; set; }
+
+    private string CssClass => new CssBuilder()
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+    private string CssStyle => new StyleBuilder()
+        .AddStyleFromAttributes(AdditionalAttributes)
+        .Build();
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.OpenElement(0, Component);
+        builder.AddMultipleAttributes(1, AdditionalAttributes);
+        builder.AddAttribute(2, "class", CssClass);
+        builder.AddAttribute(3, "style", CssStyle);
+        builder.AddContent(4, ChildContent);
+        builder.CloseElement();
+    }
+}
