1d059c7bc638b27d30560384d27d37cb4beb6dcc
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-09-10 14:33:36 +0200

    Input Group component
    - Code Behind
    - XML Doc comments
    - HtmlElement usage

---
+++ src/blatternfly/Components/InputGroup/InputGroupText.razor.cs
@@ -0,0 +1,33 @@
+namespace Blatternfly.Components;
+
+public partial class InputGroupText : ComponentBase
+{
+    /// <summary>
+    /// Additional attributes that will be applied to the component.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object> AdditionalAttributes { get; set; }
+
+    /// <summary>
+    /// Content rendered inside the component.
+    /// </summary>
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    /// <summary>
+    /// Component that wraps the input group text.
+    /// </summary>
+    [Parameter]
+    public string Component { get; set; } = "span";
+
+    /// <summary>
+    /// Input group text variant.
+    /// </summary>
+    [Parameter]
+    public InputGroupTextVariant Variant { get; set; } = InputGroupTextVariant.Default;
+
+    private string CssClass => new CssBuilder("pf-c-input-group__text")
+        .AddClass("pf-m-plain", Variant is InputGroupTextVariant.Plain)
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+}
--- src/blatternfly/Components/InputGroup/InputGroupTextVariant.cs
+++ src/blatternfly/Components/InputGroup/InputGroupTextVariant.cs
@@ -1,7 +1,17 @@
 ﻿namespace Blatternfly.Components;
 
+/// <summary>
+/// Input group text variants.
+/// <summary>
 public enum InputGroupTextVariant
 {
+    /// <summary>
+    /// Default.
+    /// </summary>
     Default,
+
+    /// <summary>
+    /// Plain.
+    /// </summary>
     Plain
 }
