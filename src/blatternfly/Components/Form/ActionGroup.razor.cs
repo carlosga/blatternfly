30fd523af060d3a76b70964eabc1c1776af62b0b
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-09-10 09:32:40 +0200

    Form Component
    - Code behind
    - XML doc comments
    - HtmlElement usage

---
+++ src/blatternfly/Components/Form/ActionGroup.razor.cs
@@ -0,0 +1,21 @@
+namespace Blatternfly.Components;
+
+public partial class ActionGroup : ComponentBase
+{
+    /// <summary>
+    /// Additional attributes that will be applied to the component.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object> AdditionalAttributes { get; set; }
+
+    /// <summary>
+    /// Content rendered inside the component.
+    /// </summary>
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    private string CssClass => new CssBuilder("pf-c-form__group")
+        .AddClass("pf-m-action")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+}
