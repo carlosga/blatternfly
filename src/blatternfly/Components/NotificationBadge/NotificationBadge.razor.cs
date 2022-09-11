f238ed19258b8c6acd7e6d02d28bac9e38996bb4
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-09-11 09:36:44 +0200

    Notification badge component
    - Code behind
    - XML doc comments

---
+++ src/blatternfly/Components/NotificationBadge/NotificationBadge.razor.cs
@@ -0,0 +1,56 @@
+namespace Blatternfly.Components;
+
+public partial class NotificationBadge : ComponentBase
+{
+    /// <summary>
+    /// Additional attributes that will be applied to the component.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object> AdditionalAttributes { get; set; }
+
+    /// <summary>
+    /// Content rendered inside the component.
+    /// </summary>
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    /// <summary>
+    /// Determines the variant of the notification badge.
+    /// </summary>
+    [Parameter]
+    public NotificationBadgeVariant Variant { get; set; }
+
+    /// <summary>
+    /// A number displayed in the badge alongside the icon.
+    /// </summary>
+    [Parameter]
+    public int? Count { get; set; }
+
+    /// <summary>
+    /// Adds accessible text to the Notification Badge.
+    /// </summary>
+    [Parameter]
+    public string AriaLabel { get; set; }
+
+    /// <summary>
+    /// Icon to display for attention variant.
+    /// </summary>
+    [Parameter]
+    public RenderFragment AttentionIcon { get; set; }
+
+    /// <summary>
+    /// Icon do display in notification badge.
+    /// </summary>
+    [Parameter]
+    public RenderFragment Icon { get; set; }
+
+    /// <summary>
+    /// </summary>
+    [Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+    private string CssClass => new CssBuilder("pf-c-notification-badge")
+        .AddClass("pf-m-attention", Variant is NotificationBadgeVariant.Attention)
+        .AddClass("pf-m-read"     , Variant is NotificationBadgeVariant.Read)
+        .AddClass("pf-m-unread"   , Variant is NotificationBadgeVariant.Unread)
+        .Build();
+}
--- src/blatternfly/Components/NotificationBadge/NotificationBadgeVariant.cs
+++ src/blatternfly/Components/NotificationBadge/NotificationBadgeVariant.cs
@@ -1,8 +1,14 @@
 namespace Blatternfly.Components;
 
+/// <summary>Notification badge variants</summary>
 public enum NotificationBadgeVariant
 {
+    /// <summary>Read.</summary>
     Read,
+
+    /// <summary>Unread.</summary>
     Unread,
+
+    /// <summary>Attention.</summary>
     Attention
 }
