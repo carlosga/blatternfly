52244b71817026c6fcbe6a8c523c61f3b42c88c2
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-09-14 20:13:58 +0200

    TextArea component
    - XMl doc comments

--- src/blatternfly/Components/TextInput/TextInputTypes.cs
+++ src/blatternfly/Components/TextInput/TextInputTypes.cs
@@ -1,16 +1,38 @@
 namespace Blatternfly.Components;
 
+/// <summary>Text input types.</summary>
 public enum TextInputTypes
 {
+    /// <summary>Text.</summary>
     Text,
+
+    /// <summary>Date.</summary>
     Date,
+
+    /// <summary>Datetime local.</summary>
     DatetimeLocal,
+
+    /// <summary>Email.</summary>
     Email,
+
+    /// <summary>Month.</summary>
     Month,
+
+    /// <summary>Number.</summary>
     Number,
+
+    /// <summary>Password.</summary>
     Password,
+
+    /// <summary>Search.</summary>
     Search,
+
+    /// <summary>Tel.</summary>
     Tel,
+
+    /// <summary>Time.</summary>
     Time,
+
+    /// <summary>Url.</summary>
     Url
 }
