5013a91f76b3765f2bdd838d6a6ada819b8fd1db
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-09-14 20:29:24 +0200

    TextInputGroup component
    - Formatting changes

--- src/blatternfly/Components/TextInputGroup/TextInputGroupUtilities.razor.cs
+++ src/blatternfly/Components/TextInputGroup/TextInputGroupUtilities.razor.cs
@@ -2,13 +2,13 @@ namespace Blatternfly.Components;
 
 public partial class TextInputGroupUtilities : ComponentBase
 {
-  /// <summary>Additional attributes that will be applied to the component.</summary>
-  [Parameter(CaptureUnmatchedValues = true)] public IReadOnlyDictionary<string, object> AdditionalAttributes { get; set; }
+    /// <summary>Additional attributes that will be applied to the component.</summary>
+    [Parameter(CaptureUnmatchedValues = true)] public IReadOnlyDictionary<string, object> AdditionalAttributes { get; set; }
 
-  /// <summary>Content rendered inside the component.</summary>
-  [Parameter] public RenderFragment ChildContent { get; set; }
+    /// <summary>Content rendered inside the component.</summary>
+    [Parameter] public RenderFragment ChildContent { get; set; }
 
-  private string CssClass => new CssBuilder("pf-c-text-input-group__utilities")
-    .AddClassFromAttributes(AdditionalAttributes)
-    .Build();
+    private string CssClass => new CssBuilder("pf-c-text-input-group__utilities")
+      .AddClassFromAttributes(AdditionalAttributes)
+      .Build();
 }
