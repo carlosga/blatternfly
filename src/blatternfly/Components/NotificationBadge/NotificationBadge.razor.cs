bb30c426047b7c825d4736e519f473a294bc1087
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-10-01 09:29:36 +0200

    PatternFly sync : Notification Badge

--- src/blatternfly/Components/NotificationBadge/NotificationBadge.razor.cs
+++ src/blatternfly/Components/NotificationBadge/NotificationBadge.razor.cs
@@ -8,23 +8,23 @@ public partial class NotificationBadge : ComponentBase
     /// <summary>Content rendered inside the component.</summary>
     [Parameter] public RenderFragment ChildContent { get; set; }
 
-    /// <summary>Determines the variant of the notification badge.</summary>
-    [Parameter] public NotificationBadgeVariant Variant { get; set; }
-
-    /// <summary>A number displayed in the badge alongside the icon.</summary>
-    [Parameter] public int? Count { get; set; }
-
     /// <summary>Adds accessible text to the Notification Badge.</summary>
     [Parameter] public string AriaLabel { get; set; }
 
     /// <summary>Icon to display for attention variant.</summary>
     [Parameter] public RenderFragment AttentionIcon { get; set; }
 
+    /// <summary>A number displayed in the badge alongside the icon.</summary>
+    [Parameter] public int? Count { get; set; }
+
     /// <summary>Icon do display in notification badge.</summary>
     [Parameter] public RenderFragment Icon { get; set; }
 
     /// <summary></summary>
     [Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+    /// <summary>Determines the variant of the notification badge.</summary>
+    [Parameter] public NotificationBadgeVariant Variant { get; set; }
 
     private string CssClass => new CssBuilder("pf-c-notification-badge")
         .AddClass("pf-m-attention", Variant is NotificationBadgeVariant.Attention)
